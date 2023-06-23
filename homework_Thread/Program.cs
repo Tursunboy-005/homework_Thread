71b4e3bf4514a2ed9b29c68daf5ab84ebc6c5eb4
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-23 09:32:06 +0500

    improve solution

--- homework_Thread/Program.cs
+++ homework_Thread/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace homework_Thread
+﻿using System.Xml.Linq;
+
+namespace homework_Thread
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] toplam = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // 1-chi thread: Elementlarni faylga yozish
+        Thread thread1 = new Thread(() =>
+        {
+            using (StreamWriter writer = new StreamWriter("elements.txt"))
+            {
+                foreach (int element in toplam)
+                {
+                    writer.WriteLine(element);
+                }
+            }
+
+            Console.WriteLine("Elementlar faylga yozildi.");
+        });
+
+            // 2-chi thread: Elementlarni ekranga chiqarish
+            Thread thread2 = new Thread(() =>
+            {
+                Console.WriteLine("To'plamdagi elementlar:");
+                foreach (int element in toplam)
+                {
+                    Console.WriteLine(element);
+                }
+            });
+
+            // Threadlarni ishga tushiramiz
+            thread1.Start();
+            thread2.Start();
+
+            // Threadlarning tugashini kuzatish
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Dastur tugadi.");
         }
     }
 }
